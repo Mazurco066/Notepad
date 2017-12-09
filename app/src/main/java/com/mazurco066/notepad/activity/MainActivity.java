4df53954be695bc10c01a7de2c78eaba3c5e1032
Gabriel ribeiro <mazurco066@gmail.com>
2017-12-09 17:26:05 -0200

    Added Nnote Activity

--- app/src/main/java/com/mazurco066/notepad/activity/MainActivity.java
+++ app/src/main/java/com/mazurco066/notepad/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mazurco066.notepad.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +11,10 @@ import android.widget.ListView;
 
 import com.mazurco066.notepad.R;
 import com.mazurco066.notepad.dao.NoteDAO;
+import com.mazurco066.notepad.model.Note;
+import com.mazurco066.notepad.util.DatabaseCreator;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
 
     //Definindo Atributos
+    private List<Note> notes;
     private NoteDAO dao;
 
     @Override
@@ -74,8 +80,30 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //Método que será responsávelpore xecutar as ações de recuperar dados de uma nota
+    //Método que será responsável por executar as ações de recuperar dados de uma nota
     private void writeNote() {
+
+        //Instanciando uma nova nota com id de não salva para validação
+        Note note = new Note();
+        note.setId(-1);
+
+        //Abrindo Activity de edição para nova nota
+        openNoteActivity(note);
+
+    }
+
+    //Método para Abrir Activity de Escrever/Visualizar Nota
+    private void openNoteActivity(Note note) {
+
+        //Instanciando intent para ir para prox activity
+        Intent noteActivity = new Intent(getApplicationContext(), NoteActivity.class);
+        noteActivity.putExtra(DatabaseCreator.FIELD_ID, note.getId());
+        noteActivity.putExtra(DatabaseCreator.FIELD_DATE, note.getDate());
+        noteActivity.putExtra(DatabaseCreator.FIELD_TITLE, note.getTitle());
+        noteActivity.putExtra(DatabaseCreator.FIELD_CONTENT, note.getContent());
+
+        //Iniciando nova activity
+        startActivity(noteActivity);
 
     }
 
--- app/src/main/java/com/mazurco066/notepad/activity/NoteActivity.java
+++ app/src/main/java/com/mazurco066/notepad/activity/NoteActivity.java
@@ -2,14 +2,68 @@ package com.mazurco066.notepad.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.mazurco066.notepad.R;
 
 public class NoteActivity extends AppCompatActivity {
 
+    //Definindo Componentes
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Implementação padrão do método onCreate
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        //Instanciando Componentes
+        toolbar = findViewById(R.id.mainToolbar);
+
+        //Configurando a Toolbar
+        toolbar.setTitle("New Note");
+        toolbar.setNavigationIcon(R.drawable.ic_keyboard_arrow_left);
+        this.setSupportActionBar(toolbar);
+    }
+
+    //Sobrescrevendo método de inflar menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        //Inflando menu na activity
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.note_menu, menu);
+        return  true;
+    }
+
+    //Sobrescrevendo método para ver qual botão do menu foi pressionado
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //Verificando qual botão foi pressionado
+        switch (item.getItemId()) {
+
+            //Caso pressionou botão salvar
+            case R.id.action_save:
+                break;
+
+            //Caso pressionou botão deletar
+            case R.id.action_delete:
+                break;
+
+            //Caso pressionou botão voltar
+            case R.id.action_back:
+                finish();
+                break;
+
+        }
+
+        //Retorno padrão do método
+        return super.onOptionsItemSelected(item);
     }
 }
