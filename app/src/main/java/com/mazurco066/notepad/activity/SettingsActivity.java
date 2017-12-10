c571b214746e324809f32ff7bd1d5078ead1052d
Gabriel ribeiro <mazurco066@gmail.com>
2017-12-10 19:29:55 -0200

    Added Settings Activity

---
+++ app/src/main/java/com/mazurco066/notepad/activity/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.mazurco066.notepad.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mazurco066.notepad.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
