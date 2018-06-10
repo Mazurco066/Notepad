10189734eb127db4d8cf7a5f0daacb5f5c52d0bb
Gabriel Ribeiro <mazurco066@gmail.com>
2018-06-10 17:49:09 -0300

    Changed Divider Color

--- app/src/main/java/com/mazurco066/notepad/ui/fragments/ListsFragment.java
+++ app/src/main/java/com/mazurco066/notepad/ui/fragments/ListsFragment.java
@@ -2,6 +2,7 @@ package com.mazurco066.notepad.ui.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
